6f3522a64b46c0fdb8b9cd99aeea0cf10a431b70
bryleero <beroby@crimson.ua.edu>
2024-04-22 22:30:01 -0500

    YAY

--- Clients/Resources/Scripts/index.js
+++ Clients/Resources/Scripts/index.js
@@ -1,3 +1,53 @@
 function handleOnLoad(){
 populateList();
 }
+
+function populateForm(){
+    document.getElementById("bookTitle").value = myBook.title;
+    document.getElementById("bookAuthor").value = myBook.author;
+    document.getElementById("bookGenre").value = myBook.genre;
+    document.getElementById("BookAvlb").value = myBook.numAvlb;
+    document.getElementById("bookIsbn").value = myBook.isbn;
+    document.getElementById("bookLength").value = myBook.length;
+    document.getElementById("bookCover").value = myBook.cover;
+    var html = "<img class = \"coverArt\" src= \"" +myBook.cover + "\"></img";
+    document.getElementById("picBox").innerHTML = html;
+}
+
+function hideButtons(){
+    document.getElementById("newButton").style.display = "none";
+    document.getElementById("editButton").style.display = "none";
+    document.getElementById("deleteButton").style.display = "none";
+    document.getElementById("rentButton").style.display = "none";
+    document.getElementById("returnButton").style.display = "none";
+}
+
+function makeEditable(){
+    document.getElementById("bookTitle").readOnly=false;
+    document.getElementById("bookAuthor").readOnly=false;
+    document.getElementById("bookGenre").readOnly=false;
+    document.getElementById("BookAvlb").readOnly=false;
+    document.getElementById("bookIsbn").readOnly=false;
+    document.getElementById("bookLength").readOnly=false;
+    document.getElementById("bookCover").readOnly=false;
+}
+
+function blankFields(){
+    document.getElementById("bookTitle").value="";
+    document.getElementById("bookAuthor").value="";
+    document.getElementById("bookGenre").value="";
+    document.getElementById("BookAvlb").value="";
+    document.getElementById("bookIsbn").value="";
+    document.getElementById("bookLength").value="";
+    document.getElementById("bookCover").value="";
+}
+
+function makeReadOnly(){
+    document.getElementById("bookTitle").readOnly=true;
+    document.getElementById("bookAuthor").readOnly=true;
+    document.getElementById("bookGenre").readOnly=true;
+    document.getElementById("BookAvlb").readOnly=true;
+    document.getElementById("bookIsbn").readOnly=true;
+    document.getElementById("bookLength").readOnly=true;
+    document.getElementById("bookCover").readOnly=true;
+}
